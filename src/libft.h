bbae05a9d7c0c677e89d92ecb7126bb79599e624
Rafael de Lima Franco <rde-lima@student.42sp.org.br>
2021-05-23 15:49:54 -0300

    adicionado novas funções

--- src/libft.h
+++ src/libft.h
@@ -6,7 +6,7 @@
 /*   By: rde-lima <rde-lima@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/16 21:42:06 by rde-lima          #+#    #+#             */
-/*   Updated: 2021/05/23 04:31:47 by rde-lima         ###   ########.fr       */
+/*   Updated: 2021/05/23 14:32:17 by rde-lima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 # define LIBFT_H
 
 unsigned int	ft_strlen(const char *str);
-unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size);
-unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
+unsigned int	ft_strlcpy(char *dest, const char *src, unsigned int size);
+unsigned int	ft_strlcat(char *dest, const char *src, unsigned int size);
 char			*ft_strnstr(const char *str, const char *to_find);
+int				ft_strncmp(const char *s1, const char *s2, unsigned int n);
 int				ft_isalpha(int c);
 int				ft_isdigit(int c);
 int				ft_isalnum(int c);
