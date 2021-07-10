373542437ad8384c1c87440988c8d44fc7b9d5c4
Rafael de Lima Franco <rde-lima@student.42sp.org.br>
2021-07-10 16:41:42 -0300

    corrigindo memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: rde-lima <rde-lima@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/30 13:37:06 by rde-lima          #+#    #+#             */
-/*   Updated: 2021/06/04 22:10:12 by rde-lima         ###   ########.fr       */
+/*   Updated: 2021/07/10 16:41:29 by rde-lima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	unsigned char		*odest;
-	const unsigned char	*osrc;
+	char	*odest;
+	char	*osrc;
 
-	odest = dest;
-	osrc = src;
+	if (dest == src)
+		return (dest);
+	odest = (char *)dest;
+	osrc = (char *)src;
 	while (n-- > 0)
 		*odest++ = *osrc++;
 	return (dest);
