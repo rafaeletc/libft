5dcfaa3b4cb2ffec8a6f50e5b7874b0e754ff328
Rafael de Lima Franco <rde-lima@student.42sp.org.br>
2021-07-10 18:55:00 -0300

    apagando comentários

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rde-lima <rde-lima@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/16 21:42:06 by rde-lima          #+#    #+#             */
-/*   Updated: 2021/07/09 15:54:00 by rde-lima         ###   ########.fr       */
+/*   Updated: 2021/07/10 18:54:30 by rde-lima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-// Mandatory part 1
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
@@ -38,7 +37,6 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
-// Mandatory part 2
 void	*ft_calloc(size_t num, size_t size);
 char	*ft_strdup(const char *str1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
@@ -52,21 +50,5 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-// Bonus
-//typedef struct s_list
-//{
-//	void			*content;
-//	struct s_list	*next;
-//}					t_list;
-//
-//t_list	*ft_lstnew(void *content);
-//void	ft_lstadd_front(t_list **lst, t_list *new);
-//int		ft_lstsize(t_list *lst);
-//t_list	*ft_lstlast(t_list *lst);
-//void	ft_lstadd_back(t_list **lst, t_list *new);
-//void	ft_lstdelone(t_list *lst, void (*del)(void *));
-//void	ft_lstclear(t_list **lst, void (*del)(void *));
-//void	ft_lstiter(t_list *lst, void (*f)(void *));
-//t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
