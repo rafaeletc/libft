7321aef43c1dada766f5c79d6c042425e9e72efc
Rafael de Lima Franco <rafaeletc@users.noreply.github.com>
2021-11-20 22:40:21 +0100

    new tests

--- tests/test_putnbr_fd.c
+++ tests/test_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: rde-lima <rde-lima@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 21:42:30 by rde-lima          #+#    #+#             */
-/*   Updated: 2021/11/20 21:48:16 by rde-lima         ###   ########.fr       */
+/*   Updated: 2021/11/20 21:58:55 by rde-lima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 
 int	main(void)
 {
+	ft_putnbr_fd(0, 0);
+	printf("\n");
+	ft_putnbr_fd(10, 0);
+	printf("\n");
+	ft_putnbr_fd(-42, 0);
+	printf("\n");
 	ft_putnbr_fd(INT_MAX, 0);
 	printf("\n");
 	ft_putnbr_fd(INT_MIN, 0);
