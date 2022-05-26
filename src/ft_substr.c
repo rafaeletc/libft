7983790a5067a66bb921e42459e445824f926615
Rafael de Lima Franco <rafaeletc@users.noreply.github.com>
2022-05-26 16:18:01 -0300

    optmized for 'get_next_line' project

--- src/ft_substr.c
+++ src/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: rde-lima <rde-lima@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/03 23:17:38 by rde-lima          #+#    #+#             */
-/*   Updated: 2021/11/22 03:00:52 by rde-lima         ###   ########.fr       */
+/*   Updated: 2022/05/26 16:16:26 by rde-lima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,24 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*str;
 	size_t	size;
+	char	*str;
 
 	if (!s)
 		return (NULL);
 	if (start > ft_strlen(s))
 		return (ft_strdup(""));
-	if (start < ft_strlen(s))
-		size = ft_strlen(s) - start;
-	else
-		size = 0;
-	if (size > len)
-		size = len;
-	str = malloc(sizeof(char) * (size + 1));
+	if (start + len > ft_strlen(s))
+		len = ft_strlen(s) - start;
+	str = (char *)malloc(sizeof(char) * len + 1);
 	if (!str)
 		return (NULL);
-	ft_strlcpy(str, s + start, size + 1);
+	size = 0;
+	while (s[start + size] && size < len)
+	{
+		str[size] = s[start + size];
+		++size;
+	}
+	str[size] = '\0';
 	return (str);
 }
